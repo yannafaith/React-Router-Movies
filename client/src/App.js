d290cbf4547dc4439812f91535b975b0a644b5ff
yannafaith <yannafaith97@gmail.com>
2019-01-22 02:04:01 -0700

    added Routes in App / Part I MVP done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import {Route} from 'react-router-dom';
 
 export default class App extends Component {
   constructor() {
@@ -20,10 +21,13 @@ export default class App extends Component {
 
   render() {
     return (
+
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
+        <Route path='/' exact component={MovieList}/>
+        <Route path='/movies/:id' component={Movie}/>
+      </div> 
+
     );
   }
 }
